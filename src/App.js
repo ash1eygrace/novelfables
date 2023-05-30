611edc89eaab2f56bb6b6002e8be52a9dc9203be
ash1eygrace <ashleygrobicheau@gmail.com>
2023-05-30 06:56:21 -0600

    Add Google Books API search feature and replace list with results from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import BookList from './Components/BookList';
 
-const books = [
+const initialBooks = [
   {
     id: 1,
     image: 'https://www.goodreads.com/book/cover/show/403769.The_Hitchhiker_s_Guide_to_the_Galaxy',
@@ -23,9 +24,35 @@ const books = [
 ];
 
 const App = () => {
+  const [books, setBooks] = useState(initialBooks);
+  const [header, setHeader] = useState("Book List");
+
+  const searchBooks = async (e) => {
+    e.preventDefault();
+    const query = e.target.elements.search.value;
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.REACT_APP_API_KEY}`
+    );
+    const books = response.data.items.map((book) => {
+      const image = book.volumeInfo.imageLinks
+        ? book.volumeInfo.imageLinks.thumbnail
+        : ''; 
+      const title = book.volumeInfo.title;
+      const author = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : '';
+      return { id: book.id, image, title, author };
+    });
+    setBooks(books);
+    setHeader("Search Results");
+  };
+
+
   return (
     <div>
-      <h1>Book List</h1>
+      <h1>{header}</h1>
+      <form onSubmit={searchBooks}>
+        <input type="text" name="search" placeholder="Search for books" />
+        <button type="submit">Search</button>
+      </form>
       <BookList books={books} />
     </div>
   );
